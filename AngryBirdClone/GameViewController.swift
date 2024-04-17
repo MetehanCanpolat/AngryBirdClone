4e7d7280cef4a878967d9c0c1bfd055e3ac0446e
MetehanCanpolat <metehancanpolat@outlook.com>
2024-04-17 16:56:11 +0300

    finish

--- AngryBirdClone/GameViewController.swift
+++ AngryBirdClone/GameViewController.swift
@@ -29,7 +29,6 @@ class GameViewController: UIViewController {
             // sağ altta fps i felan gösteriyor;
             view.showsFPS = true
             view.showsNodeCount = true
-            
             view.showsPhysics = true
         }
     }
